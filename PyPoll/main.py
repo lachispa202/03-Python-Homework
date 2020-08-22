015734ad8f62e3a8fa463c04a273f8108852f823
Tobias Judd <tobias_judd@hotmail.com>
2020-08-22 09:53:41 -0500

    PyPoll completed with output files. Homework is done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,7 +11,7 @@ import csv
 csvpath = os.path.join("..", "Resource", "election_data.csv")
 
 # Variables for the number of votes. Equals number of rows, excluding header. 
-votes = []
+votes_ID = []
 
 # Creating a dictionary for the different candidates
 name = []
@@ -31,36 +31,24 @@ with open(csvpath, 'r') as csvfile:
         # print(row)
 
 # Calculation for total number of votes, based on number of Voter ID
-        votes.append(row[0])       
-        total_votes=len(votes)
+        votes_ID.append(row[0])       
+        total_votes=len(votes_ID)
 
-# Candidate information including number of votes received
+# Candidate information including number of votes received by candidate
         name = row[2]
-        
+     
         if name not in election_candidate.keys():
             election_candidate[name]=1
         else:
             election_candidate[name] += 1
    
-
+# To break down the results by candidate, number of votes, and percentage. Includes formatting. 
         candidate_name = ""
-        for candidate in election_candidate.items():
-            candidate_name += '\n'.join([candidate_name, str(candidate) + str(election_candidate[candidate]) + " ) "]),'\n'
-            # candidate_name += '\n'.join([candidate_name, str(candidate) + '{:.1%}'.format(election_candidate/total_votes)]),'\n'
-            
-            # percent_votes = (election_candidate/total_votes)
+        for candidate in election_candidate.keys():
+            candidate_name += '\n'.join([str(candidate) + ":    " + '{:.1%}'.format((election_candidate[candidate])/(total_votes)) + "  of vote received, with a total number of votes being: (" + str(election_candidate[candidate]) + ")", '\n'])
 
-            # percent_votes_formated = '{:.1%}'.format(percent_votes)
-            
-            # candidates_information += '\n'.join([(candidate_name) + "      " + (percent_votes_formated) + "     ( "   + " )"'\n'])
-            # ] +  '{:,.2f}'.format(election_candidate) +        + '{:,.2f}'.format(election_candidate) + 
-            # candidate_name = '\n'.join([str(candidate) + "     ( "   + " )"'\n'])
-            # percent_votes = '\n'.join(election_candidate/total_votes)'\n'
-
+# Calculation to determine the winning of the election
         Winner = max(election_candidate.keys(), key =(lambda k: election_candidate[k]))
-
-# Calculation to get the votes per candidate
-
 
 # # Output Summary, including number formatting
     print()
@@ -75,29 +63,33 @@ with open(csvpath, 'r') as csvfile:
     print("Election Winner:")
     print( Winner)
     print("-----------------------------------------------")
-# # Sequence to export results
-# # Specify the file to write to
-# output_path = os.path.join("Financial_Summary.txt")
+# Sequence to export results
+# Specify the file to write to
+output_path = os.path.join("Election_Summary.txt")
 
-# with open("Financial_Summary.txt", 'w') as text_file:
+with open("Election_Summary.txt", 'w') as text_file:
 
 # # Write the following:
-#     text_file.write('\n')
-#     text_file.write("Summary of Financial Analysis")
-#     text_file.write('\n')
-#     text_file.write("-----------------------------------------------")
-#     text_file.write('\n')
-#     text_file.write("Total Months:                      " + (str(total_months)))
-#     text_file.write('\n')
-#     text_file.write("Total:                           $ " + '{:,.2f}'.format(total_profit_loss))
-#     text_file.write('\n')
-#     text_file.write("Average Change:                  $" + '{:,.2f}'.format(average_change))
-#     text_file.write('\n')
-#     text_file.write("Greatest Increase in Profits:    $ " + '{:,.2f}'.format(greatest_increase_profits) + "  in month " + (str(greatest_increase_profits_date)))
-#     text_file.write('\n')
-#     text_file.write("Greatest Decrease in Profits:    $" + '{:,.2f}'.format(greatest_decrease_profits) + "  in month " + (str(greatest_decrease_profits_date)))
-#     text_file.write('\n')
-#     text_file.write("-----------------------------------------------")
-#     text_file.write('\n')
+    text_file.write('\n')
+    text_file.write("Election Summary")
+    text_file.write('\n')
+    text_file.write("-----------------------------------------------")
+    text_file.write('\n')
+    text_file.write("Total Votes:                      " + str(total_votes))
+    text_file.write('\n')
+    text_file.write("-----------------------------------------------")
+    text_file.write('\n')
+    text_file.write("Elections Results for Each Candidate:")
+    text_file.write('\n')
+    text_file.write((candidate_name))
+    text_file.write('\n')
+    text_file.write("-----------------------------------------------")
+    text_file.write('\n')
+    text_file.write("Election Winner:")
+    text_file.write('\n')
+    text_file.write( Winner)
+    text_file.write('\n')
+    text_file.write("-----------------------------------------------")
+    text_file.write('\n')
 
 # # End of program
